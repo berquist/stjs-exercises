9f1edf04178a33b3c30fa575d4e58492d6ef8abc
Eric Berquist <eric.berquist@gmail.com>
2021-12-30 13:11:04 -0500

    start async-programming exercises

---
+++ async-programming/tracing-promise-execution.js
@@ -0,0 +1,3 @@
+// Promise.resolve('hello');
+// Promise.resolve('hello').then(result => console.log(result));
+const p = new Promise((resolve, reject) => resolve('hello')).then(result => console.log(result));
