eb03779c3d6727701d3ffe673f63df903b772f27
Eric Berquist <eric.berquist@gmail.com>
2022-01-22 10:32:21 -0500

    default tolerance

--- unit-test/asserts.js
+++ unit-test/asserts.js
@@ -1,13 +1,16 @@
 import assert from 'assert'
 
-const assertApproxEqual = (left, right, tol, message) => {
+const assertApproxEqual = (left, right, message, tol) => {
+    if (tol === undefined) {
+        tol = 0.1;
+    }
     if (Math.abs(left - right) > tol) {
         throw new assert.AssertionError({message});
     }
 };
 
 // throws exception
-// assertApproxEqual(1.0, 2.0, 0.01, 'Values are too far apart');
+// assertApproxEqual(1.0, 2.0, 'Values are too far apart');
 
 // does not throw
-assertApproxEqual(1.0, 2.0, 10.0, 'Large margin of error');
+assertApproxEqual(1.0, 2.0, 'Large margin of error', 1.0);
